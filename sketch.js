7fb226d01615e4132f9f69d6626fed1bb2ad8195
Ståle Gjelsten <stalegjelsten@gmail.com>
2021-09-06 11:33:07 +0200

    deleted comments

--- sketch.js
+++ sketch.js
@@ -1,34 +1,33 @@
 let myShape;
-let div;
+let liveeqn;
 let eqn;
 let plainTextEqn;
 
 function setup() {
   createCanvas(400, 400)
   stroke(60)
-  // frameRate(10)
   strokeWeight(3)
   noFill()
   noLoop()
-  div = createP('');
-  div.position(width/2-170, height);
+  liveeqn = createP('');
+  liveeqn.position(width/2-170, height);
   myShape = new SuperShape(1, 1, 1, 4, 1, 1, 100)
   let gui = new dat.GUI()
   gui.add(myShape, "m", 0, 20, 0.1).onChange(redraw)
-  gui.add(myShape, "radius", 0, 200, 5).onChange(redraw)
   gui.add(myShape, "n1", 0, 10, 0.1).onChange(redraw)
   gui.add(myShape, "n2", 0, 10, 0.1).onChange(redraw)
   gui.add(myShape, "n3", 0, 10, 0.1).onChange(redraw)
   gui.add(myShape, "a", 1, 10, 1).onChange(redraw)
   gui.add(myShape, "b", 1, 10, 1).onChange(redraw)
   gui.add(myShape, "circumnavigations", 1, 20, 1).onChange(redraw)
+  gui.add(myShape, "radius", 0, 200, 5).onFinishChange(redraw)
   eqn = createP('<img src="https://cdn.jsdelivr.net/gh/stalegjelsten/2d-supershapes@master/svgs/510d395cbc490aab4c993e0d3c901f22.svg?invert_in_darkmode" align=middle width=309.62586269999997pt height=55.85645175pt/></p>')
   eqn.style('align', 'center')
   eqn.position(width/2-170, height+70)
   plainTextEqn = createP()
   plainTextEqn.style('width', width+"px")
+  plainTextEqn.style('margin', "15px")
   plainTextEqn.position(0,height+150)
-  // myShape.changed(draw)
 }
 
 class SuperShape {
@@ -67,8 +66,8 @@ class SuperShape {
       vertex(x, y)
     }
     endShape(CLOSE)
-    katex.render(this.equation, div.elt)
-    plainTextEqn.html("<code>" + this.equation + "</code>")
+    katex.render(this.equation, liveeqn.elt)
+    plainTextEqn.html("LaTeX code for the equation: <br /><code>" + this.equation + "</code>")
   }
 }
 
@@ -77,18 +76,3 @@ function draw() {
   translate(width / 2, height / 2)
   myShape.display()
 }
-
-// function mouseMoved() {
-//   redraw(60);
-// }
-
-// function keyPressed() {
-//   redraw(60);
-// }
-
-// function mouseIsPressed() {
-//   redraw(60);
-// }
-// function mouseReleased() {
-//   redraw(60);
-// }
