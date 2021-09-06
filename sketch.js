3a21f6b8b1cb42227827dfe74798519ff4764e85
Ståle Gjelsten <stalegjelsten@gmail.com>
2021-09-06 20:38:29 +0200

    added save image option and made TeX equation compatible with direct paste into geogebra

--- sketch.js
+++ sketch.js
@@ -1,33 +1,41 @@
-let myShape;
-let liveeqn;
-let eqn;
-let plainTextEqn;
+let myShape
+let liveeqn
+let eqn
+let plainTeXEqn
+let asciiEqn
+let canv
 
 function setup() {
-  createCanvas(400, 400)
+  canv = createCanvas(400, 400)
   stroke(60)
   strokeWeight(3)
   noFill()
   noLoop()
-  liveeqn = createP('');
+  let btn = { add:function(){saveCanvas(canv, "supershape", "png")}}
+  liveeqn = createP('')
   liveeqn.position(width/2-170, height);
   myShape = new SuperShape(1, 1, 1, 4, 1, 1, 100)
   let gui = new dat.GUI()
   gui.add(myShape, "m", 0, 20, 0.1).onChange(redraw)
-  gui.add(myShape, "n1", 0, 10, 0.1).onChange(redraw)
-  gui.add(myShape, "n2", 0, 10, 0.1).onChange(redraw)
-  gui.add(myShape, "n3", 0, 10, 0.1).onChange(redraw)
-  gui.add(myShape, "a", 1, 10, 1).onChange(redraw)
-  gui.add(myShape, "b", 1, 10, 1).onChange(redraw)
+  gui.add(myShape, "n1", -5, 10, 0.1).onChange(redraw)
+  gui.add(myShape, "n2", -5, 10, 0.1).onChange(redraw)
+  gui.add(myShape, "n3", -5, 10, 0.1).onChange(redraw)
+  gui.add(myShape, "a", -5, 10, 0.1).onChange(redraw)
+  gui.add(myShape, "b", -5, 10, 0.1).onChange(redraw)
   gui.add(myShape, "circumnavigations", 1, 20, 1).onChange(redraw)
   gui.add(myShape, "radius", 0, 200, 5).onFinishChange(redraw)
+  gui.add(btn, "add").name("Save image")
   eqn = createP('<img src="https://cdn.jsdelivr.net/gh/stalegjelsten/2d-supershapes@master/svgs/510d395cbc490aab4c993e0d3c901f22.svg?invert_in_darkmode" align=middle width=309.62586269999997pt height=55.85645175pt/></p>')
   eqn.style('align', 'center')
   eqn.position(width/2-170, height+70)
-  plainTextEqn = createP()
-  plainTextEqn.style('width', width+"px")
-  plainTextEqn.style('margin', "15px")
-  plainTextEqn.position(0,height+150)
+  plainTeXEqn = createP()
+  plainTeXEqn.style('width', width+"px")
+  plainTeXEqn.style('margin', "15px")
+  plainTeXEqn.position(0,height+220)
+  asciiEqn = createP()
+  asciiEqn.style('width', width+"px")
+  asciiEqn.style('margin', "15px")
+  asciiEqn.position(0,height+150)
 }
 
 class SuperShape {
@@ -42,14 +50,17 @@ class SuperShape {
     this.radius = radius
     this.circumnavigations = 1
     this.equation = ""
+    this.asciieq = ""
   }
 
   calcRadius(theta) {
     let p1 = abs(1 / this.a * cos(theta * this.m / 4)) ** this.n2
     let p2 = abs(1 / this.b * sin(theta * this.m / 4)) ** this.n3
     let ans = pow(p1 + p2, 1 / this.n1)
-    this.equation = "r(\\theta) = \\left({\\left\\lvert \\frac{\\cos{\\left(\\frac{"+ this.m.toPrecision(2) + "}{4} \\theta\\right)}}{" + this.a + "} \\right\\rvert ^{" + this.n2.toPrecision(2) + "}" 
+    this.equation = "r = \\left({\\left\\lvert \\frac{\\cos{\\left(\\frac{"+ this.m.toPrecision(2) + "}{4} \\theta\\right)}}{" + this.a + "} \\right\\rvert ^{" + this.n2.toPrecision(2) + "}" 
     this.equation += "+ \\left\\lvert \\frac{\\sin{\\left(\\frac{"+ this.m.toPrecision(2) + "}{4} \\theta\\right)}}{" + this.a + "} \\right\\rvert ^{" + this.n3.toPrecision(2) + "}}\\right)^{-\\frac{1}{" + this.n1.toPrecision(2) + "}}" 
+    this.asciieq = "r(theta) = ( |(cos((" + this.m.toPrecision(2) + " theta) / 4)) / " + this.a.toPrecision(2) + "|^(" + this.n2.toPrecision(2) + ") + |(sin((" + this.m.toPrecision(2) + " theta) / 4)) / " + this.b.toPrecision(2) + " |^(" + this.n3.toPrecision(2) + "))^(-1/(" + this.n1.toPrecision(2) + ")"
+
     if (ans === 0) {
       return 0
     }
@@ -67,7 +78,8 @@ class SuperShape {
     }
     endShape(CLOSE)
     katex.render(this.equation, liveeqn.elt)
-    plainTextEqn.html("LaTeX code for the equation: <br /><code>" + this.equation + "</code>")
+    asciiEqn.html("Plain ASCII code for the equation: <br /><code>" + this.asciieq + "</code>")
+    plainTeXEqn.html("LaTeX code for the equation (can be pasted into GeoGebra): <br /><code>" + this.equation + "</code>")
   }
 }
 
